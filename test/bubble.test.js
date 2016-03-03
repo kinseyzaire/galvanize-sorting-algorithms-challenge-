a944de96abc6704afa5919cc56d0cebe4efa4902
Kinsey Ashby <kinseyzaire@gmail.com>
2016-03-03 10:22:25 -0700

    initial solution

--- test/bubble.test.js
+++ test/bubble.test.js
@@ -15,22 +15,22 @@ describe('#bubbleSort', function() {
     var checkedResults = checkResults(sortResults, expected).length
     assert.equal(checkedResults, 0);
   });
-  xit('sorts an array of three', function () {
+  it('sorts an array of three', function () {
     var input = [5, 2, 1];
     var expected = [1, 2, 5];
     var sortResults = bubbleSort(input);
     var checkedResults = checkResults(sortResults, expected).length
     assert.equal(checkedResults, 0);
   });
-  xit('sorts an array of 8', function () {
+  it('sorts an array of 8', function () {
     var input = [5, 2, 4, 3, 8, 10, 1, 0];
     var expected = [0, 1, 2, 3, 4, 5, 8, 10];
     var sortResults = bubbleSort(input);
     var checkedResults = checkResults(sortResults, expected).length
     assert.equal(checkedResults, 0);
   });
-  xit('handles duplicates', function () {
-    var input = [5, 2, 4, 3, 8, 10, 1, 0, 2];
+  it('handles dupes', function () {
+    var input = [5, 2, 4, 3, 8, 10, 1, 2, 0];
     var expected = [0, 1, 2, 2, 3, 4, 5, 8, 10];
     var sortResults = bubbleSort(input);
     var checkedResults = checkResults(sortResults, expected).length
